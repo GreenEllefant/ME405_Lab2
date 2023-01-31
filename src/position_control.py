8a0a81cf34cb9d66334bb74b24b9eab31cab9997
c4324r <baconlord123456@gmail.com>
2023-01-31 09:06:04 -0800

    class structure changed

--- src/position_control.py
+++ src/position_control.py
@@ -2,17 +2,26 @@ import utime
 
 class Position_Control:
     
-    def __init__(self, gain, setpoint):
+    def __init__(self, gain, setpoint, encoder, motor):
         self.gain = gain
         self.setpoint = setpoint
         self.values = [[], []]
+        self.encoder = encoder
+        self.motor = motor
         self.time = utime.ticks_ms()
 
-    def run(self, measured):
-        return self.gain * (self.setpoint - measured)
+    def run(self, setpoint):
+        self.setpoint = setpoint
+        self.values[0].append(utime.ticks_ms() - self.time)
+        self.values[1].append(self.encoder.read())
+        self.motor.set_duty_cycle(self.gain * (self.setpoint - self.encoder.read()))
 
     def set_setpoint(self, setpoint):
         self.setpoint = setpoint
     
     def set_Kp(self, gain):
         self.gain = gain
+
+    def print_values(self):
+        for i in range(0, len(self.values[0])):
+            print(str(self.values[0][i]) + "," + str(self.values[1][i]))
